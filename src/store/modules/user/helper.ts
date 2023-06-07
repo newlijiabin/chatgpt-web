629780b7fb98e11cbd696976a72dcd819b8d7429
newlijiabin <38779696+newlijiabin@users.noreply.github.com>
2023-06-07 15:01:19 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://s1.ax1x.com/2023/06/06/pCiduUP.jpg',
+      name: 'FltanAI',
+      description: '科技改变生活',
     },
   }
 }
